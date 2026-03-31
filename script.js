d96303873c3af188ba11d6c9b01f48e5c4fcd930
vivek9078 <vivek1907004@gmail.com>
2026-03-31 16:18:07 +0530

    Frontend

---
+++ script.js
@@ -0,0 +1,10 @@
+function createUser() {
+  const name = document.getElementById("name").value;
+  const role = document.getElementById("role").value;
+
+  console.log("User:", name, role);
+}
+
+function getUsers() {
+  console.log("Fetching users...");
+}
