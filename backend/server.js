899e605f672365beb549c47ff2fd224e3349e041
vivek9078 <vivek1907004@gmail.com>
2026-05-26 20:23:10 +0530

    Fixed CORS and API deployment

--- backend/server.js
+++ backend/server.js
@@ -45,5 +45,6 @@ app.get("/", (req, res) => {
 // ---- Start Server ----
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
+
     console.log(`Server running on http://localhost:${PORT}`);
 });
